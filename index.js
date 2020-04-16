6c8c47ae1db88eb7ace4dcf3dfb6c539923d6648
Varun Govil <varun.govil@trioptima.com>
2020-04-16 19:02:07 +0200

    updated puppeteer to latest

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 // @ts-ignore
-const config = require('./config.json')
+const config = require('./config.json');
 
-let interval
-let browser
+let interval;
+let browser;
 
 function getSelectedLocationUrl() {
-    const selectedUrl = config.browser.URLS[config.browser.preferred]
+    const selectedUrl = config.browser.URLS[config.browser.preferred];
     if (!selectedUrl) {
-        console.log(`Could not find URL for selected location ${config.browser.preferred}`)
+        console.log(`Could not find URL for selected location ${config.browser.preferred}`);
     }
-    return selectedUrl
+    return selectedUrl;
 }
 
 async function initBrowser(headless, url) {
@@ -19,55 +19,55 @@ async function initBrowser(headless, url) {
         defaultViewport: null,
         args: ['--start-maximized'],
         timeout: 0,
-    })
-    const page = await browser.newPage()
+    });
+    const page = await browser.newPage();
     await page.goto(url, {
         waitUntil: 'domcontentloaded',
-    })
-    return page
+    });
+    return page;
 }
 
 // Launch new page and pre-fill information
 async function launchAppointmentForm() {
-    const page = await initBrowser(false, getSelectedLocationUrl())
-    let elementHandles = await page.$$('div.subsection input')
+    const page = await initBrowser(false, getSelectedLocationUrl());
+    let elementHandles = await page.$$('div.subsection input');
     if (elementHandles.length === 5) {
-        await elementHandles[0].type(config.form.firstName)
-        await elementHandles[1].type(config.form.lastName)
-        await elementHandles[2].type(config.form.phone)
-        await elementHandles[3].type(config.form.email)
-        await elementHandles[4].type(config.form.email)
+        await elementHandles[0].type(config.form.firstName);
+        await elementHandles[1].type(config.form.lastName);
+        await elementHandles[2].type(config.form.phone);
+        await elementHandles[3].type(config.form.email);
+        await elementHandles[4].type(config.form.email);
     }
 }
 
 // Checks for availability of date
 async function checkMigrationsverket() {
-    const page = await initBrowser(true, getSelectedLocationUrl())
+    const page = await initBrowser(true, getSelectedLocationUrl());
     const dateAvailable = await page.evaluate((UNAVAILABLE) => {
-        let errorText = document.querySelector('span.feedbackPanelERROR')
+        let errorText = document.querySelector('span.feedbackPanelERROR');
         if (errorText) {
-            let value = errorText.innerHTML
-            return value !== UNAVAILABLE
+            let value = errorText.innerHTML;
+            return value !== UNAVAILABLE;
         }
-        return true
-    }, config.messages.unavailable)
+        return true;
+    }, config.messages.unavailable);
 
     if (dateAvailable) {
-        await closeBrowser()
-        clearInterval(interval)
-        await launchAppointmentForm()
+        await closeBrowser();
+        clearInterval(interval);
+        await launchAppointmentForm();
     } else {
-        console.log(`Date unavailable, will check again in ${config.browser.interval / 1000} seconds`)
-        await closeBrowser()
+        console.log(`Date unavailable, will check again in ${config.browser.interval / 1000} seconds`);
+        await closeBrowser();
     }
 }
 
 async function closeBrowser() {
-    await browser.close()
+    await browser.close();
 }
 
-;(async () => {
+(async () => {
     interval = setInterval(() => {
-        checkMigrationsverket()
-    }, config.browser.interval)
-})()
+        checkMigrationsverket();
+    }, config.browser.interval);
+})();
